632de150bc4a78dc334a661a29acaa1fe2e06acc
rayan <134594995+rayan2162@users.noreply.github.com>
2024-06-07 18:25:39 +0600

    temporary routes added for frontend

--- sfa/routes/web.php
+++ sfa/routes/web.php
@@ -1,32 +1,49 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\WeatherController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-
+// Temporary routes for frontend
+// ----------------------------------------------------------------
 Route::get('/', function () {
     return view('indexPage');
 });
 
+Route::get('/notice', function () {
+    return view('notice.noticePage');
+});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/crop', function () {
+    return view('crop.cropPage');
+});
+
+Route::get('/agroforestry', function () {
+    return view('agroforestry.agroForestryPage');
+});
+
+Route::get('/livestock', function () {
+    return view('livestock.livestockPage');
+});
+
+Route::get('/silo', function () {
+    return view('silo.siloPage');
+});
+
+Route::get('/equipment', function () {
+    return view('equipment.equipmentPage');
+});
+
+Route::get('/create', function () {
+    return view('demoCreate');
 });
 
 
-Route::get('/weather', [WeatherController::class, 'index']);
 
+// Backend done
+// ----------------------------------------------------------------
+Route::get('/weather', [WeatherController::class, 'index']);
 
 Auth::routes();
 
