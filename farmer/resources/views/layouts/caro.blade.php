97cf550f300296684577d4c2bad43f059c57adb1
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 16:42:36 +0600

    carosel layout added

---
+++ farmer/resources/views/layouts/caro.blade.php
@@ -0,0 +1,107 @@
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="6"></li>
+    </ol>
+
+    <div class="carousel-inner">
+
+      {{-- 1st --}}
+      <div class="carousel-item active">
+          <img src="https://images.unsplash.com/photo-1466801182732-9e4ea054c094?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" style="max-height: 80vh; object-fit: cover;  filter: blur(1.8px);" alt="Maximize Your Crop Yield">
+        <div class="carousel-caption ">
+          <h2 style="text-shadow: 10em " >Maximize Your Crop Yield</h2>
+          <p style="text-shadow: 13em" > Increase your farm's productivity with our comprehensive crop management solutions. </p> 
+          <p style="text-shadow: 13em"  class="d-none d-md-block" >
+            From planting to harvest, we provide tools and techniques to optimize growth, minimize waste, and maximize yield. Our intuitive platform helps you manage irrigation, and implement efficient farming practices for sustainable and profitable results.</p>
+        </div>
+      </div>
+      
+      {{-- 2nd --}}
+      <div class="carousel-item">
+        <img src="https://images.unsplash.com/photo-1598616914356-0e09322ed3b8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fGdyYXBlJTIwZmFybXxlbnwwfDB8MHx8fDI%3D" class="d-block w-100" style="max-height: 80vh; object-fit: cover;  filter: blur(1.8px);" alt="Second slide">
+        <div class="carousel-caption ">
+            <h2 style="text-shadow: 10em" >Enhance Orchard & Agroforestry Operations</h2>
+            <p style="text-shadow: 13em" > Elevate your orchard and agroforestry management with our specialized solutions tailored to tree-based agriculture. </p> 
+            <p style="text-shadow: 13em"  class="d-none d-md-block">
+              Whether you're cultivating fruit orchards, nut groves, or timber stands, our comprehensive platform offers advanced tools and resources to optimize productivity and sustainability. With our integrated technology, you can streamline every aspect of your tree-based production and achieve long-term success.</p>
+          </div>
+        </div>
+        
+        {{-- 3rd --}}
+        <div class="carousel-item">
+          <img src="https://images.unsplash.com/photo-1716016435588-f5db7da707d5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" style="max-height: 80vh; object-fit: cover;  filter: blur(1.8px);" alt="Second slide">
+          <div class="carousel-caption ">
+              <h2 style="text-shadow: 10em" >Efficient Crop Storage Solutions</h2>
+              <p style="text-shadow: 13em" >  Streamline your crop management process with our advanced silo storage solutions.</p> 
+              <p style="text-shadow: 13em"  class="d-none d-md-block" >
+                Our innovative technology ensures proper storage management to preserve crop quality and reduce spoilage. From grain drying to automated inventory management, we offer scalable solutions tailored to your farm's needs, allowing you to store and manage crops with ease.</p>
+            </div>
+          </div>
+
+      {{-- 4th --}}
+      <div class="carousel-item">
+        <img src="https://images.unsplash.com/photo-1596733430284-f7437764b1a9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTEyfHxjb3dzfGVufDB8MHwwfHx8Mg%3D%3D" class="d-block w-100" style="max-height: 80vh; object-fit: cover;  filter: blur(1.8px);" alt="Second slide">
+        <div class="carousel-caption ">
+            <h2 style="text-shadow: 10em" >Optimize Livestock Health and Performance</h2>
+            <p style="text-shadow: 13em" >Enhance the well-being and productivity of your livestock with our comprehensive management solutions. </p> 
+            <p style="text-shadow: 13em"  class="d-none d-md-block">
+              From health monitoring to feeding schedules, our platform provides real-time insights and actionable data to ensure optimal care and performance. With advanced analytics and customizable alerts, you can proactively address issues and maximize profitability.
+            </p>
+          </div>
+        </div>
+
+        {{-- 5th --}}
+      <div class="carousel-item">
+        <img src="https://images.unsplash.com/photo-1530267981375-f0de937f5f13?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dHJhY3RvcnxlbnwwfDB8MHx8fDI%3D" class="d-block w-100" style="max-height: 80vh; object-fit: cover;  filter: blur(1.5px); -webkit-filter: blur(1.65x);" alt="Second slide">
+        <div class="carousel-caption ">
+            <h2 style="text-shadow: 10em" >Efficient Equipment Storage Solutions</h2>
+            <p style="text-shadow: 13em" >Keep your farm running smoothly with our reliable equipment storage solutions</p> 
+            <p style="text-shadow: 13em"  class="d-none d-md-block">
+              From tractors to implements, our application's organization ensure easy access and keep track of your valuable assets. With trackig storage and convenient managing, you can protect your equipment investment and optimize operational efficiency.
+            </p>
+          </div>
+        </div>
+        
+        {{-- 6th --}}
+      <div class="carousel-item">
+        <img src="https://images.unsplash.com/uploads/14116603688211a68546c/30f8f30b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" style="max-height: 80vh; object-fit: cover;  filter: blur(1.8px);" alt="Second slide">
+        <div class="carousel-caption ">
+            <h2 style="text-shadow: 10em" >Stay Ahead with Accurate Weather Forecasts</h2>
+            <p style="text-shadow: 13em" > Make informed decisions for your farm with our reliable weather news service.</p> 
+            <p style="text-shadow: 13em"  class="d-none d-md-block">
+              Our application provides up-to-date forecasts and insightful analysis tailored to your location and agricultural needs. Whether planning planting schedules or managing harvest operations, our forecasts help you mitigate risks and maximize productivity in any weather condition.
+            </p>
+          </div>
+        </div>
+
+        {{-- 7th --}}
+      <div class="carousel-item">
+        <img src="https://images.unsplash.com/photo-1605000797499-95a51c5269ae?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" style="max-height: 80vh; object-fit: cover;  filter: blur(1.8px);" alt="Second slide">
+        <div class="carousel-caption ">
+            <h2 style="text-shadow: 10em" > Enhance Your Farming Skills</h2>
+            <p style="text-shadow: 13em" > Elevate your farming expertise with our comprehensive training programs and workshops. </p> 
+            <p style="text-shadow: 13em"  class="d-none d-md-block">
+              From crop management techniques to livestock care practices, our experienced instructors offer hands-on training and valuable insights to help you succeed in modern agriculture. Stay informed about the latest industry trends and best practices to continually improve your farming skills and achieve your goals.
+            </p>
+          </div>
+        </div>
+
+    </div>
+
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+
+  </div>
