4a26a300dfc65833c238ba3d9da0698258314838
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 22:09:19 +0600

    unnecessary field removed

--- farmer/resources/views/layouts/demoForm.blade.php
+++ farmer/resources/views/layouts/demoForm.blade.php
@@ -5,27 +5,21 @@
       </div>
 
     <div class="form-group">
-        <label for="text">Email address</label>
+        <label for="text text-white ">Email address</label>
         <input type="text" class="form-control" id="text" placeholder="text">
       </div>
 
       <div class="form-group">
-        <label for="text">Email address</label>
+        <label for="text text-white ">Email address</label>
         <input type="text" class="form-control" id="text" placeholder="text">
       </div>
 
       <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    </div>
-
-    <div class="form-group">
-        <label for="email" class="col-md-4 col-form-label text-md-end" style="color: #4B6F44; font-weight:bolder">{{ __('Email Address') }}</label>
-        <div>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-    </div>
+        <label for="text text-white ">Email address</label>
+        <input type="text" class="form-control" id="text" placeholder="text">
+      </div>
+      
 
     <br>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn" style="background:#4B6F44; color:white">Submit</button>
   </form>
