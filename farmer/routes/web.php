837b5279381e57cf9a3e5e779dcc201ab1cfc126
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 16:52:42 +0600

    temporary routes for frontend debug

--- farmer/routes/web.php
+++ farmer/routes/web.php
@@ -1,22 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Landing Page
+Route::get('/', function () {
+    return view('indexPage');
+});
+
+// Feature page
+Route::get('/crop', function () {
+    return view('crop.cropPage');
+});
+
+Route::get('/agroforestry', function () {
+    return view('agroforestry.agroForestryPage');
+});
+
+Route::get('/equipment', function () {
+    return view('equipment.equipmentPage');
+});
+
+Route::get('/livestock', function () {
+    return view('livestock.livestockPage');
+});
+
+Route::get('/notice', function () {
+    return view('notice.noticePage');
+});
+
+Route::get('/silo', function () {
+    return view('silo.siloPage');
+});
