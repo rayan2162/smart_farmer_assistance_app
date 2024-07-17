75de3d8bb831b7dc7fd28289455cad7e113d62c2
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 16:50:11 +0600

    hero component added

---
+++ farmer/resources/views/components/hero.blade.php
@@ -0,0 +1,15 @@
+<div class="jumbotron" style="background-image: url('{{ $backgroundImage }}'); background-size: cover; background-position: center; height: 80vh;">
+    <div class="row" style="background: rgba(0, 0, 0, 0.5); padding: 20px; border-radius: 10px;">
+        <!-- Left Column -->
+            <div class="col-md-8" style="padding: 20px; border-radius: 10px;">
+                <h1 class="display-5 text-white">{{ $title }}</h1>
+                <p class="lead text-white">{{ $leadText }}</p>
+                <hr class="my-4 text-dark">
+                <p class="text-white">{{ $description }}</p>
+            </div>
+        <!-- Right Column -->
+        <div class="col-md-4">
+                @yield('heroContent')
+        </div>
+    </div>
+</div>
