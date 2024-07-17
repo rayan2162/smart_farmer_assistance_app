0749baf8dcff6fdb2030b885400057d4bf5c06db
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 16:43:31 +0600

    jumbotron layout added

---
+++ farmer/resources/views/layouts/jumbo.blade.php
@@ -0,0 +1,11 @@
+<div class="jumbotron" style="background-image: url('https://images.unsplash.com/photo-1484759288640-783b22c95d54?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-size: cover; background-position: center; height: 70vh;">
+      <div style="background: rgba(0, 0, 0, 0.5); padding: 20px; border-radius: 10px;">
+          <h1 class="display-5 text-white">Transform Your Farming Experience with us!</h1>
+          <p class="lead text-white">You've seen the features, join with us and many more to experience the benefits right now for free!.</p>
+          <hr class="my-4 text-white">
+          <p class="text-white">Our Smart Farmer Assistant Application integrates advanced technology and practical tools to streamline your farm operations. From crop management to weather forecasts, we provide everything you need for a successful farming experience.</p>
+          <p class="lead">
+              <a class="btn btn-lg" href="{{ route('register') }}" role="button" style="background:#4B6F44; color:white">Get Started</a>
+          </p>
+      </div>
+  </div>
