dde128825c4ecdc75a967f58e4ca568d0a8176ad
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 16:43:09 +0600

    demoForm for placeholder layout added

---
+++ farmer/resources/views/layouts/demoForm.blade.php
@@ -0,0 +1,31 @@
+<form >
+    <div class="form-group">
+        <label for="text">Email address</label>
+        <input type="text" class="form-control" id="text" placeholder="text">
+      </div>
+
+    <div class="form-group">
+        <label for="text">Email address</label>
+        <input type="text" class="form-control" id="text" placeholder="text">
+      </div>
+
+      <div class="form-group">
+        <label for="text">Email address</label>
+        <input type="text" class="form-control" id="text" placeholder="text">
+      </div>
+
+      <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+
+    <div class="form-group">
+        <label for="email" class="col-md-4 col-form-label text-md-end" style="color: #4B6F44; font-weight:bolder">{{ __('Email Address') }}</label>
+        <div>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+    </div>
+
+    <br>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
