a38635bcd3b37ab6235a45286fab7191ed824629
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 16:46:29 +0600

    landing page

---
+++ farmer/resources/views/indexPage.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+
+@section('carousel')
+    @include('layouts.caro')
+@endsection
+
+
+@section('content')
+    @include('layouts.textArea')
+@endsection
+
+
+@section('jumbotron')
+    @include('layouts.jumbo')
+@endsection
