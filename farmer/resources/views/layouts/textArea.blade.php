29b1d55b55d1e1c50500a371386c29b1e9e7c80c
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 16:44:35 +0600

    textarea layout added

---
+++ farmer/resources/views/layouts/textArea.blade.php
@@ -0,0 +1,110 @@
+<br>
+<br>
+<div class="container marketing">
+    <!-- First row of icons -->
+    <div class="row">
+
+
+        {{-- 1 --}}
+        <div class="col-lg-3 text-center">
+            <div class="icon-circle">
+                <span class="material-symbols-outlined">rice_bowl</span>
+            </div>
+            <br>
+            <h2 style="color: #4B6F44; text-shadow:black 5rem" >Farm Production Management</h2>
+            <p style="color: #4B6F44; text-shadow:black 5rem" >
+                Maximize your farm's potential with our intuitive farm management tools. From crop planning to harvest tracking, streamline your operations for increased productivity and profitability. Stay organized and informed and utilize resources.
+            </p>
+        </div>
+
+
+        {{-- 2 --}}
+        <div class="col-lg-3 text-center">
+            <div class="icon-circle">
+                <span class="material-symbols-outlined">nutrition</span>
+            </div>
+            <br>
+            <h2 style="color: #4B6F44; text-shadow:black 5rem" >Agroforestry Solutions</h2>
+            <p style="color: #4B6F44; text-shadow:black 5rem" >
+                Enhance your agroforestry with our tailored solutions for tree-based agriculture. Boost productivity and sustainability in orchards, groves, or timber stands using advanced tools and resources. Achieve long-term success with our management application.
+            </p>
+        </div>
+
+
+        {{-- 3 --}}
+        <div class="col-lg-3 text-center">
+            <div class="icon-circle">
+                <span class="material-symbols-outlined">warehouse</span>
+            </div>
+            <br>
+            <h2 style="color: #4B6F44; text-shadow:black 5rem" >Silo Crop Management</h2>
+            <p style="color: #4B6F44; text-shadow:black 5rem" >
+                Efficiently manage your crop storage with our silo solutions. Ensure proper storage tracking to preserve crop quality and reduce spoilage. By nventory management, streamline your crop management process and protect your harvest investment.
+            </p>
+        </div>
+
+
+        {{-- 4 --}}
+        <div class="col-lg-3 text-center">
+            <div class="icon-circle">
+                <span class="material-symbols-outlined">cruelty_free</span>
+            </div>
+            <br>
+            <h2 style="color: #4B6F44; text-shadow:black 5rem" >Livestock Management</h2>
+            <p style="color: #4B6F44; text-shadow:black 5rem" >
+                Enhance the health and performance of your livestock with our management solutions. Monitor health metrics, track feeding schedules with our applcation. With advanced analytics manage your livestock for maximum profitability.
+            </p>
+        </div>
+    </div><!-- /.row -->
+
+
+<br><br>
+
+
+
+    <!-- Second row of icons -->
+    <div class="row justify-content-center">
+
+
+        {{-- 5 --}}
+        <div class="col-lg-3 text-center">
+            <div class="icon-circle">
+                <span class="material-symbols-outlined">agriculture</span>
+            </div>
+            <br>
+            <h2 style="color: #4B6F44; text-shadow:black 5rem" >Equipment Storage Solutions</h2>
+            <p style="color: #4B6F44; text-shadow:black 5rem" >
+                Keep your farm running smoothly with our reliable equipment storage solutions. From tractors to implements, organize your valuable assets. With convenient storage tracking protect your equipment investment and optimize operational efficiency.
+            </p>
+        </div>
+
+
+        {{-- 6 --}}
+        <div class="col-lg-3 text-center">
+            <div class="icon-circle">
+                <span class="material-symbols-outlined">rainy</span>
+            </div>
+            <br>
+            <h2 style="color: #4B6F44; text-shadow:black 5rem" >Accurate Weather Forecast</h2>
+            <p style="color: #4B6F44; text-shadow:black 5rem" >
+                Make informed farm decisions with our reliable weather forecast service. Receive up-to-date, location-specific forecasts to plan planting, manage harvests, and mitigate risks, ensuring optimal productivity in changing weather conditions.
+            </p>
+        </div>
+
+
+        {{-- 7 --}}
+        <div class="col-lg-3 text-center">
+            <div class="icon-circle">
+                <span class="material-symbols-outlined">notifications_active</span>
+            </div>
+            <br>
+            <h2 style="color: #4B6F44; text-shadow:black 5rem" >Government / Training Notices</h2>
+            <p style="color: #4B6F44; text-shadow:black 5rem" >
+                Boost your farming expertise with local training programs and workshops. Learn the latest management techniques and  livestock care from experienced instructors. Stay updated with our notice system and continually improve your skills.
+            </p>
+        </div>
+    </div>
+
+</div>
+<br>
+<br>
