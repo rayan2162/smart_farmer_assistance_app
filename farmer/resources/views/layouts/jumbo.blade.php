9fa0ade3d64cc461df926244af6266f7bbd87800
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 17:26:42 +0600

    text modified

--- farmer/resources/views/layouts/jumbo.blade.php
+++ farmer/resources/views/layouts/jumbo.blade.php
@@ -1,11 +1,9 @@
 <div class="jumbotron" style="background-image: url('https://images.unsplash.com/photo-1484759288640-783b22c95d54?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-size: cover; background-position: center; height: 70vh;">
       <div style="background: rgba(0, 0, 0, 0.5); padding: 20px; border-radius: 10px;">
-          <h1 class="display-5 text-white">Transform Your Farming Experience with us!</h1>
-          <p class="lead text-white">You've seen the features, join with us and many more to experience the benefits right now for free!.</p>
+          <h1 class="display-5 text-white">Connect with us</h1>
+          <p class="lead text-white">Want to learn? Any inquiries? Feel free to send email to learn more. </p>
           <hr class="my-4 text-white">
-          <p class="text-white">Our Smart Farmer Assistant Application integrates advanced technology and practical tools to streamline your farm operations. From crop management to weather forecasts, we provide everything you need for a successful farming experience.</p>
-          <p class="lead">
-              <a class="btn btn-lg" href="{{ route('register') }}" role="button" style="background:#4B6F44; color:white">Get Started</a>
+              <a class="btn btn-lg" href="" role="button" style="background:#4B6F44; color:white">Send an Email</a>
           </p>
       </div>
   </div>
