9359e139dbd32d0177ad147a4963897822257730
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-17 09:48:24 +0600

    reste

--- sfa/resources/views/components/hero.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="jumbotron" style="background-image: url('{{ $backgroundImage }}'); background-size: cover; background-position: center; height: 70vh;">
-    <div style="background: rgba(0, 0, 0, 0.5); padding: 20px; border-radius: 10px;">
-        <h1 class="display-5 text-white">{{ $title }}</h1>
-        <p class="lead text-white">{{ $leadText }}</p>
-        <hr class="my-4 text-dark">
-        <p class="text-white">{{ $description }}</p>
-    </div>
-</div>
