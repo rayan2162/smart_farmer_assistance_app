2ef8699134ef27263f2252016bac47bf085f7a95
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-27 11:21:28 +0600

    silo page added

---
+++ farmer/resources/views/silo/siloPage.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('carousel')
+    @component('components.hero')
+        @slot('backgroundImage', 'https://images.unsplash.com/photo-1457414104202-9d4b4908f285?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGlsZSUyMG9mJTIwc2Fja3N8ZW58MHwwfDB8fHwy')
+        @slot('title', 'Efficiently manage your crop storage')
+        @slot('leadText', 'Ensure proper storage tracking to preserve crop quality and reduce spoilage.')
+        @slot('description', 'By nventory management, streamline your crop management process and protect your harvest investment. ')
+    
+        @section('heroContent')
+            @include('layouts.demoForm')
+        @endsection
+    @endcomponent
+@endsection
+
+
+
+@section('content')
+    @include('./demoText')
+@endsection
