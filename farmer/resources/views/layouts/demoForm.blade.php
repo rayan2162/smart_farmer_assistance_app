bb550700daad6adcfdee46f10654e806ce7e2718
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-27 09:45:21 +0600

    Form title and placeholder modified

--- farmer/resources/views/layouts/demoForm.blade.php
+++ farmer/resources/views/layouts/demoForm.blade.php
@@ -1,25 +1,27 @@
 <form >
-    <div class="form-group">
-        <label for="text">Email address</label>
-        <input type="text" class="form-control" id="text" placeholder="text">
-      </div>
 
-    <div class="form-group">
-        <label for="text text-white ">Email address</label>
-        <input type="text" class="form-control" id="text" placeholder="text">
-      </div>
+  <div class="form-group">
+    <label class="text-white" for="text ">Demo Title here</label>
+    <input type="text" class="form-control" id="text" placeholder="Demo placeholder text here">
+  </div>
 
-      <div class="form-group">
-        <label for="text text-white ">Email address</label>
-        <input type="text" class="form-control" id="text" placeholder="text">
-      </div>
+  <div class="form-group">
+    <label class="text-white" for="text ">Demo Title here</label>
+    <input type="text" class="form-control" id="text" placeholder="Demo placeholder text here">
+  </div>
+  
+  <div class="form-group">
+    <label class="text-white" for="text ">Demo Title here</label>
+    <input type="text" class="form-control" id="text" placeholder="Demo placeholder text here">
+  </div>
 
-      <div class="form-group">
-        <label for="text text-white ">Email address</label>
-        <input type="text" class="form-control" id="text" placeholder="text">
-      </div>
+  <div class="form-group">
+    <label class="text-white" for="text ">Demo Title here</label>
+    <input type="text" class="form-control" id="text" placeholder="Demo placeholder text here">
+  </div>
       
-
     <br>
+
     <button type="submit" class="btn" style="background:#4B6F44; color:white">Submit</button>
+
   </form>
