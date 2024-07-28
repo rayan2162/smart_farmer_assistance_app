a41719e7b52f39a3d574ce82cf59f20d371ad624
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-28 12:22:30 +0600

    logout button modified

--- farmer/resources/views/layouts/app.blade.php
+++ farmer/resources/views/layouts/app.blade.php
@@ -153,12 +153,12 @@
 
 
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white font-weight-bold" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown" style="background: #4B6F44">
+                                    <a style="background: #4B6F44" class="dropdown-item text-white font-weight-bold" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
