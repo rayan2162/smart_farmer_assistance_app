8f835f9606da381e3a1d867802acaf40fc90c057
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-30 19:46:11 +0600

    font and broder modified

--- farmer/resources/views/crop/cropForm.blade.php
+++ farmer/resources/views/crop/cropForm.blade.php
@@ -2,49 +2,49 @@
     <form action="{{ route('crop.store') }}" method="POST">
         @csrf
         <div class="form-group row">
-            <label class="col-sm-6 col-form-label text-white" for="name">Crop Name</label>
+            <label style="font-size: 1.15em" class="col-sm-6 col-form-label text-white" for="name">Crop Name</label>
             <div class="col-sm-6">
-                <input type="text" class="form-control" id="name" name="name" placeholder="Enter crop name" required>
+                <input style="border: #4B6F44 solid" type="text" class="form-control" id="name" name="name" placeholder="Enter crop name" required>
             </div>
         </div>
         
         <div class="form-group row">
-            <label class="col-sm-4 col-form-label text-white" for="type">Crop Type</label>
+            <label style="font-size: 1.15em" class="col-sm-4 col-form-label text-white" for="type">Crop Type</label>
             <div class="col-sm-8">
-                <input type="text" class="form-control" id="type" name="type" placeholder="Enter crop type" required>
+                <input style="border: #4B6F44 solid" type="text" class="form-control" id="type" name="type" placeholder="Enter crop type" required>
             </div>
         </div>
         
         <div class="form-group row">
-            <label class="col-sm-4 col-form-label text-white" for="planting_date">Planting Date</label>
+            <label style="font-size: 1.15em" class="col-sm-4 col-form-label text-white" for="planting_date">Planting Date</label>
             <div class="col-sm-8">
-                <input type="date" class="form-control" id="planting_date" name="planting_date" required>
+                <input style="border: #4B6F44 solid" type="date" class="form-control" id="planting_date" name="planting_date" required>
             </div>
         </div>
         
         <div class="form-group row">
-            <label class="col-sm-4 col-form-label text-white" for="harvest_date">Harvest Date</label>
+            <label style="font-size: 1.15em" class="col-sm-4 col-form-label text-white" for="harvest_date">Harvest Date</label>
             <div class="col-sm-8">
-                <input type="date" class="form-control" id="harvest_date" name="harvest_date">
+                <input style="border: #4B6F44 solid" type="date" class="form-control" id="harvest_date" name="harvest_date">
             </div>
         </div>
         
         <div class="form-group row">
-            <label class="col-sm-4 col-form-label text-white" for="quantity">Quantity</label>
+            <label style="font-size: 1.15em" class="col-sm-4 col-form-label text-white" for="quantity">Quantity</label>
             <div class="col-sm-8">
-                <input type="number" class="form-control" id="quantity" name="quantity" placeholder="Enter quantity" required>
+                <input style="border: #4B6F44 solid" type="number" class="form-control" id="quantity" name="quantity" placeholder="Enter quantity" required>
             </div>
         </div>
         
         <div class="form-group row">
-            <label class="col-sm-4 col-form-label text-white" for="location">Location</label>
+            <label style="font-size: 1.15em" class="col-sm-4 col-form-label text-white" for="location">Location</label>
             <div class="col-sm-8">
-                <input type="text" class="form-control" id="location" name="location" placeholder="Enter location" required>
+                <input style="border: #4B6F44 solid" type="text" class="form-control" id="location" name="location" placeholder="Enter location" required>
             </div>
         </div>
         
         <div class="form-group row">
-            <label class="col-sm-4 col-form-label text-white" for="notes">Notes</label>
+            <label style="font-size: 1.15em" class="col-sm-4 col-form-label text-white" for="notes">Notes</label>
             <div class="col-sm-8">
                 <textarea class="form-control" id="notes" name="notes" rows="2" placeholder="Additional notes"></textarea>
             </div>
