2c4f6840bee0cc1bc2530345551a02f5dbc221d8
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-30 13:24:23 +0600

    SMTP mail system's route added

--- farmer/routes/web.php
+++ farmer/routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\WeatherController;
+use App\Http\Controllers\MailController;
+
 
 
 Auth::routes();
 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 // Landing Page
 Route::get('/', function () {
@@ -39,7 +44,16 @@ Route::get('/silo', function () {
     return view('silo.siloPage');
 });
 
-// Weather Page complete routes
+
+// Weather Features Complete Routes
 Route::get('/weather', [WeatherController::class, 'show'])->name('weather.show');
 
 Route::post('/get_weather', [WeatherController::class, 'fetch'])->name('weather.fetch');
+
+
+// Mail Features Complete Routes
+Route::get('/send-email', [MailController::class, 'create']);
+
+Route::post('/send-email', [MailController::class, 'send']);
+
+
