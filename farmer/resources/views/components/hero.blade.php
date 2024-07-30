2c2384662cc7f3eda03df0818938ce480b3a5880
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-30 19:41:52 +0600

    column width fixed

--- farmer/resources/views/components/hero.blade.php
+++ farmer/resources/views/components/hero.blade.php
@@ -1,14 +1,14 @@
 <div class="jumbotron" style="background-image: url('{{ $backgroundImage }}'); background-size: cover; background-position: center; height: 80vh;">
     <div class="row" style="background: rgba(0, 0, 0, 0.5); padding: 20px; border-radius: 10px;">
         <!-- Left Column -->
-            <div class="col-md-8" style="padding: 20px; border-radius: 10px;">
+            <div class="col-md-7" style="padding: 20px; border-radius: 10px;">
                 <h1 class="display-5 text-white">{{ $title }}</h1>
                 <p class="lead text-white">{{ $leadText }}</p>
                 <hr class="my-4 text-dark">
                 <p class="text-white">{{ $description }}</p>
             </div>
         <!-- Right Column -->
-        <div class="col-md-4">
+        <div class="col-md-5">
                 @yield('heroContent')
         </div>
     </div>
