ff52c04a70f3f0e5f65e77af3f396be744a60c97
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-30 18:02:18 +0600

    footer for 2 diffrent scenario

---
+++ farmer/resources/views/layouts/footerLongPage.blade.php
@@ -0,0 +1,18 @@
+{{-- FOOTER SHORT PAGE --}}
+
+{{-- This footer should be used on pages if the pages 
+does have content covering 100% or full one page.  --}}
+
+
+<footer class="footer mt-auto py-3" style="background: #4B6F44; color: white;">
+    <div class="container">
+        <p style="float: right"><a href="#" style="color: white;">Back to top</a></p>
+        <p style="margin-bottom: 0;">&copy; 2024 Smart Farmer Assistant Application (Made with Laravel 10) <br> For 6th Semester Software Development Course.</p>
+    </div>
+</footer>
+
+
+
+
+
+
---
+++ farmer/resources/views/layouts/footerShortPage.blade.php
@@ -0,0 +1,11 @@
+{{-- FOOTER SHORT PAGE --}}
+
+{{-- This footer should be used on pages 
+if the pages doesn't have content covering 100% 
+or full one page ie. login page, registration page etc.  --}}
+
+
+    <div style="background: #4B6F44; color: white; padding: 7px; bottom:0; width:100%; position:fixed">
+        <p style="float: right"><a href="#" style="color: white;">Back to top</a></p>
+        <p style="margin-bottom: 0;">&copy; 2024 Smart Farmer Assistant Application (Made with Laravel 10) <br> For 6th Semester Software Development Course.</p>
+    </div>
