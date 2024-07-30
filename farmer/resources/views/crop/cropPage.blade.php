1d26513ee45704a1e313bbd5829e0714c8db7605
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-30 19:58:10 +0600

    table modified

--- farmer/resources/views/crop/cropPage.blade.php
+++ farmer/resources/views/crop/cropPage.blade.php
@@ -18,10 +18,10 @@
 @section('content')
     
 
-<table class="table mt-4">
+<table class="table mt-6" style="border: #4B6F44 solid">
     <thead class="thead" style="background:#4B6F44; color:white; font-weight:bolder">
         <tr>
-            <th scope="col">#</th>
+            {{-- <th scope="col">#</th> --}}
             <th scope="col">Name</th>
             <th scope="col">Type</th>
             <th scope="col">Planting Date</th>
@@ -34,8 +34,8 @@
     </thead>
     <tbody>
         @foreach($crops as $crop)
-            <tr>
-                <th scope="row">{{ $crop->id }}</th>
+            <tr style="border: #4B6F44 solid">
+                {{-- <th scope="row">{{ $crop->id }}</th> --}}
                 <td>{{ $crop->name }}</td>
                 <td>{{ $crop->type }}</td>
                 <td>{{ $crop->planting_date}}</td>
@@ -44,7 +44,7 @@
                 <td>{{ $crop->location }}</td>
                 <td>{{ $crop->notes }}</td>
                 <td>
-                    <a href="{{ route('crop.edit', $crop->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                    <a href="{{ route('crop.edit', $crop->id) }}" class="btn btn-sm" style="background-color: #4B6F44; color: white">Edit</a>
                     <form action="{{ route('crop.destroy', $crop->id) }}" method="POST" style="display:inline;">
                         @csrf
                         @method('DELETE')
