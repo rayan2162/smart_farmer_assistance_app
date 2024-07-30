8a248f50c88b6a5ee4f80e906da01109c58bab28
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-30 17:53:58 +0600

    Crop featurue added

--- farmer/resources/views/crop/cropPage.blade.php
+++ farmer/resources/views/crop/cropPage.blade.php
@@ -8,7 +8,7 @@
         @slot('description', 'From crop planning to harvest tracking, streamline your operations for increased productivity and profitability. Stay organized and informed and utilize resources. ')
     
         @section('heroContent')
-            @include('layouts.demoForm')
+            @include('crop.cropForm')
         @endsection
     @endcomponent
 @endsection
@@ -16,5 +16,49 @@
 
 
 @section('content')
-    @include('./demoText')
+    
+
+<table class="table mt-4">
+    <thead class="thead" style="background:#4B6F44; color:white; font-weight:bolder">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Type</th>
+            <th scope="col">Planting Date</th>
+            <th scope="col">Harvest Date</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Location</th>
+            <th scope="col">Notes</th>
+            <th scope="col">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($crops as $crop)
+            <tr>
+                <th scope="row">{{ $crop->id }}</th>
+                <td>{{ $crop->name }}</td>
+                <td>{{ $crop->type }}</td>
+                <td>{{ $crop->planting_date}}</td>
+                <td>{{ $crop->harvest_date}}</td>
+                <td>{{ $crop->quantity }}</td>
+                <td>{{ $crop->location }}</td>
+                <td>{{ $crop->notes }}</td>
+                <td>
+                    <a href="{{ route('crop.edit', $crop->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                    <form action="{{ route('crop.destroy', $crop->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
 @endsection
+
+{{-- @section('footer')
+    @include('')
+@endsection --}}
