3b4d2a53ef2558637db6dc8281d9fa885fb7307e
rayan <134594995+rayan2162@users.noreply.github.com>
2024-07-31 22:03:36 +0600

    admin auth added

--- farmer/app/Http/Controllers/Auth/LoginController.php
+++ farmer/app/Http/Controllers/Auth/LoginController.php
@@ -4,37 +4,33 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-        $this->middleware('auth')->only('logout');
+    }
+
+    protected function attemptLogin(\Illuminate\Http\Request $request)
+    {
+        return Auth::attempt(
+            $this->credentials($request) + ['approved' => 1],
+            $request->filled('remember')
+        );
+    }
+
+    protected function redirectTo()
+    {
+        if (Auth::user()->isAdmin) {
+            return '/admin/dashboard';
+        }
+
+        return '/home';
     }
 }
