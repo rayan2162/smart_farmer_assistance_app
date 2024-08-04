88d2e97ee0d24a85c3bb8a50744c064d6181980f
rayan <134594995+rayan2162@users.noreply.github.com>
2024-08-04 09:48:39 +0600

    modified for user to redirect to login page

--- farmer/app/Http/Controllers/Auth/RegisterController.php
+++ farmer/app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -68,5 +69,20 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        $user = $this->create($request->all());
+
+        return redirect('/login')->with('success', 'Registration successful! Please login.');
     }
 }
