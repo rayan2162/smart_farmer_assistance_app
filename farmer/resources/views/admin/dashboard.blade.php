ebbaf83db329cf6d382bb3e509d36caf247d6d7b
rayan <134594995+rayan2162@users.noreply.github.com>
2024-08-04 23:24:41 +0600

    weather widget removed

--- farmer/resources/views/admin/dashboard.blade.php
+++ farmer/resources/views/admin/dashboard.blade.php
@@ -12,10 +12,6 @@
             </div>
         <!-- Right Column -->
         <div class="col-md-8">
-            <a class="weatherwidget-io" style="border-radius: 10px" href="https://forecast7.com/en/22d3691d78/chittagong/" data-label_1="CHITTAGONG" data-label_2="WEATHER" data-font="Arial" data-icons="Climacons Animated" data-theme="weather_one" >CHITTAGONG WEATHER</a>
-            <script>
-                !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-            </script>
         </div>
 
         {{-- New row --}}
